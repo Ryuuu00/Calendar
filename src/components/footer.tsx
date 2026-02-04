3c2a58000a69093a5ae3fe286a47385a29a7fbf5
Jabez Rafael Abella <Jabezrafael1@gmail.com>
2026-02-05 01:53:41 +0800

    add date to footer and change to company name

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
+
 export default function Footer() {
+    const [currentDate, setCurrentDate] = useState(new Date());
+    
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setCurrentDate(new Date());
+        }, 1000 * 60 * 60);
+        return () => clearInterval(timer);
+    }, []);
+
+    const monthNames = [
+        "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    ];
+
+    const month = monthNames[currentDate.getMonth()];
+    const date = currentDate.getDate();
+    
     return (
         <footer>
-            <p>© 2026 My Company</p>
+            <div className="grid grid-cols-2 border-t border-gray-300">
+                <p className="text-center">{month} {date}</p>
+                <p className="text-center">© 2026 Just 4</p>
+            </div>
+
         </footer>
     );
 
